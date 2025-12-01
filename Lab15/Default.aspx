61c48ae1df1bd36c0561c1a8427a4d0b514257e0
EnriqueOctavio <engonzalez@cdspanama.org>
2025-12-01 02:26:33 -0500

    Add Lab #15 again

---
+++ Lab15/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab15._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <asp:TextBox ID="txtMensaje" runat="server" Width="300px"></asp:TextBox>
+        <br /><br />
+        <asp:Button 
+            ID="btnEnviar" 
+            runat="server" 
+            Text="Enviar" 
+            OnClick="btnEnviar_Click" 
+        />
+    </main>
+
+</asp:Content>
---
+++ Lab15/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab15
+{
+    public partial class _Default : Page   // <--- Debe llamarse _Default
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Este es el método que pide el OnClick de tu botón
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            // Leer el texto del TextBox
+            string texto = txtMensaje.Text;
+
+            // Evitar problemas con comillas y saltos de línea en JavaScript
+            texto = texto.Replace("'", "\\'")
+                         .Replace("\r", "")
+                         .Replace("\n", "\\n");
+
+            // Crear el script de JavaScript con alert()
+            string script = $"alert('{texto}');";
+
+            // Registrar el script para que se ejecute en el navegador
+            ClientScript.RegisterStartupScript(
+                this.GetType(),
+                "mostrarMensaje",
+                script,
+                true
+            );
+        }
+    }
+}
